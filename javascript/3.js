3a7eecfb69f73bb186004db642025d566529c75f
Sarang R <sarangr090205@gmail.com>
2025-04-06 13:50:58 +0530

    done

---
+++ javascript/3.js
@@ -0,0 +1,7 @@
+function isVowel(c){
+    if(c=='a'||c=='e'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U')
+        return true;
+    else
+        return false;
+}
+console.log(isVowel('a'));

913ce692fa377c956c4efefa4ec4a545638bf7bf
Sarang R <sarangr090205@gmail.com>
2025-04-06 13:54:11 +0530

    done

---
+++ javascript/5.js
@@ -0,0 +1,16 @@
+function sum(arr){
+    sum=0;
+    for(i=0,i<arr.length;i++){
+        sum+=arr[i];
+    }
+    return sum;
+}
+function multiply(arr){
+    product=1;
+    for(i=0;i<arr.length;i++){
+        product*=arr[i];
+    }
+    return product;
+}
+console.log(sum([1,2,3]));
+console.log(multiply([1,2,3]));

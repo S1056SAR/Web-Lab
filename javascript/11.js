671423ec93fd1eaaa6c0ef7592086d0a90e39042
Sarang R <sarangr090205@gmail.com>
2025-04-07 05:12:05 +0530

    done

---
+++ javascript/11.js
@@ -0,0 +1,18 @@
+function calculator(a, b, operator) {
+  switch (operator) {
+    case '+':
+      return a + b;
+    case '-':
+      return a - b;
+    case '*':
+      return a * b;
+    case '/':
+      if (b === 0) {
+        throw new Error('Division by zero');
+      }
+      return a / b;
+    default:
+      throw new Error('Invalid operator');
+  }
+}
+console.log(calculator(5, 3, '+')); 
